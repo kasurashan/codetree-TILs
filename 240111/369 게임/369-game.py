389af655dce0680051712c2a46a62439c8b43a79
kasurashan <73826816+kasurashan@users.noreply.github.com>
2024-01-11 01:21:27 +0900

    Title: 369 게임, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/369 게임/369-game.py
+++ 240111/369 게임/369-game.py
@@ -6,5 +6,9 @@ for i in range(1, n+1):
         print(0, end=' ')
     elif '3' in str(i):
         print(0, end=' ')
+    elif '6' in str(i):
+        print(0, end=' ')
+    elif '9' in str(i):
+        print(0, end=' ')
     else:
         print(i, end=' ')
